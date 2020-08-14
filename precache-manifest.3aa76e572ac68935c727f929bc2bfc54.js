bf89b8e2421f64b5bd1330f76262784c3dafa148
rohitgeddam <rohitgeddam2018@gmail.com>
2020-08-14 10:59:43 +0530

    Updates

---
+++ precache-manifest.3aa76e572ac68935c727f929bc2bfc54.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9726fc20b0c0fd5b5e23e114882a8450",
+    "url": "/community-site-client/index.html"
+  },
+  {
+    "revision": "0da3439fe5f2f1b70011",
+    "url": "/community-site-client/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "e5fcb728cbfc9cf8ba32",
+    "url": "/community-site-client/static/css/main.64182e1e.chunk.css"
+  },
+  {
+    "revision": "0da3439fe5f2f1b70011",
+    "url": "/community-site-client/static/js/2.f1dd9d9e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/community-site-client/static/js/2.f1dd9d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5fcb728cbfc9cf8ba32",
+    "url": "/community-site-client/static/js/main.98a5f877.chunk.js"
+  },
+  {
+    "revision": "d7c12d0c1ff80951c5c8",
+    "url": "/community-site-client/static/js/runtime-main.64eaee06.js"
+  },
+  {
+    "revision": "145d0c129fd7bfb10faf97149bbdc3fc",
+    "url": "/community-site-client/static/media/dscIcon.145d0c12.png"
+  }
+]);
